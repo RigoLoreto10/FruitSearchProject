2fe234f8f82ab3ef5c2dc1141382e409e6dfbba2
Rigo <rigoloreto10@gmail.com>
2023-04-24 09:54:04 -0700

    project

---
+++ starter_code/script.js
@@ -0,0 +1,54 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+	const userInput=input.value.toLowerCase();
+	fruit.filter(function(e){
+		if(e.toLowerCase().includes(userInput) && input.value !== ""){
+			results.push(e);
+		}
+	});
+	return results;
+}
+
+function searchHandler(e) {
+	//search(e.value);
+	let inputVal = e.target.value.toLowerCase();
+	const results = search(inputVal);
+	inputVal ? showSuggestions(results) : suggestions.innerText = "";
+}
+
+function removeList(){
+	let suggList = document.querySelectorAll("li")
+	suggList.forEach((item)=>{
+		item.remove();
+	})
+
+}
+
+function showSuggestions(results) {
+	suggestions.innerText="";
+	results.forEach((fruit)=>{
+		const newLi = document.createElement("li");
+		newLi.innerText=fruit;
+		suggestions.appendChild(newLi);
+	});
+	
+	// for(let i=0;i<results.length;i++){
+	// 	const newLi=document.createElement("li");
+	// 	newLi.innerText=results[i];
+	// 	suggestions.appendChild(newLi);
+	//}
+}
+
+function useSuggestion(e) {
+	input.value = e.target.innerText;
+	suggestions.innterText="";
+	removeList();
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
